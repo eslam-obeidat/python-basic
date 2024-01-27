f1d2d5289280f1b0a3ab16cb97d2408828582cf5
eslam-obeidat <146520433+eslam-obeidat@users.noreply.github.com>
2024-01-26 21:27:02 -0800

    main data type in python

---
+++ basic.py
@@ -0,0 +1,148 @@
+# ----------- int -----------
+
+x = 10
+x2=-7988934584684
+x3 = 0
+
+# ----------- flout -----------
+
+y = 0.0
+y2 = float(x)
+y3 = -85.3
+
+# ----------- complex -----------
+
+c = 8 + 2j
+c1 = 9j
+c2 = complex(8,2)
+
+# ----------- coment -----------
+
+# i'm a singel coment
+#  """ i'm a coment in spayder  """
+
+# ----------- variable name = value -----------
+
+x = 3
+y = 9.8
+t = "ok"
+
+# ----------- variable rules -----------
+ 
+number = 5
+_number = 7
+#@number = 6          (error)
+# number = 5          (error)
+#$number = 5          (error)
+#4number = 5          (error)
+
+studant_name = "Ahmad"
+Studant_Name = "Eslam"
+#studant name = "Ahmad"        (error)
+
+studantName = "Eslam"
+StudantName = "Ahmad"
+
+# ----------- boolean -----------
+
+t = True
+f = False
+
+t1 = bool(1)
+f1 = bool(0)
+t2 = bool (2)   # from 2-- to infinete
+
+# ----------- print ----------- 
+
+num1 = 10
+num2 = 5
+
+str1 = "python"
+str2 = "10"
+
+#print(num1)
+#print(num2)
+
+#print(num1,num2)
+#print ("\n")   # or print()
+#print(num1,"\n",num2,"\n")
+#print(str1,str2)
+
+"""
+        # propetis : end="" : place a break between place two printing lines 
+print(num1,num2,end=" ")
+print(str1,str2)
+
+        # propetis : sep="" : to place a separator between variables
+print(num1,num2,str1,str2,sep="_")
+
+"""
+
+# ex :  Eslam_2003*python+10
+
+name = "Eslam"
+Id = 2003
+course = "python"
+sec = 10
+
+"""
+print(name,Id,sep="_",end="*")
+print(course,sec,sep="+")
+"""
+
+
+# ----------- Arithmetic operators ----------- 
+
+"""
+operators      name
+
++              Addition
+-              Subtraction
+*              Multiplication
+/              Division
+%              Modulus
+**             Exponentiation
+//             Floor division
+"""
+"""
+x = 5
+y = 10
+g = 3.5878
+b = 7.5
+z = x + y
+
+add = x+g
+d =g+b
+sub = x-g
+mul = x*y
+div = g/y
+mod = 55%10
+exp = y**2  #y^2
+"""
+
+# ----------- Comparison operators ----------- 
+
+""" 
+operators       name
+
+==              Equal to
+!=              Not equal to
+<               Less than
+<=              Less than or equal to
+>               Greater than
+>=              Greater than or equal to
+
+"""
+
+# ----------- Comparison operators ----------- 
+
+"""
+A       B       A OR B          A       B       A AND B         A       NOT A
+TRUE    TRUE    TRUE            TRUE    TRUE    TRUE            TRUE    FALSE
+TRUE    FALSE   TRUE            TRUE    FALSE   FALSE           FALSE   TRUE
+FALSE   TRUE    TRUE            FALSE   TRUE    FALSE
+FALSE   FALSE   FALSE           FALSE   FALSE   FALSE
+
+"""
+
+# -----------  input ----------- 
